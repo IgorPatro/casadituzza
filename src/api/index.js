ec9cb452ffcd6a1d504f96a4828a789e5ce3be20
Igor Patro <tools@skyagency360.pl>
2021-11-17 19:07:07 +0100

    Added backend files upload

--- src/api/index.js
+++ src/api/index.js
@@ -5,8 +5,12 @@ const PORT = 5000
 const app = express()
 app.use(express.json())
 
+app.get('/api', (req, res) => {
+  return res.json({ response: 'This is API!' })
+})
+
 app.get('/api/test', (req, res) => {
-  return res.json({ response: 'This is my response mate!' })
+  return res.json({ response: 'This is test route!' })
 })
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
