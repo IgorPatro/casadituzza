0b6f27cdd347d46a626ab8548c736d15b477fb01
Igor Patro <tools@skyagency360.pl>
2021-11-19 10:52:53 +0100

    Created enviroment configuration

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -6,9 +6,11 @@ const PORT = 5000
 const app = express()
 app.use(express.json())
 
-app.get('/api', (req, res) => {
-  func()
+app.get('/', (_req, res) => {
+  return res.send('This is API home route!')
+})
 
+app.get('/api', (_req, res) => {
   return res.json({ response: func() })
 })
 
